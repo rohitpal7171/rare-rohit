2669ff7acad68f9b3078e74a984ae544362da66c
Rohit Singh Pal <rohit.pal7171@gmail.com>
2026-05-29 23:35:21 +0530

    fix(config): package version mismatch issue fixed

--- apps/wedding-website/vite.config.ts
+++ apps/wedding-website/vite.config.ts
@@ -1,6 +1,6 @@
 import path from 'path'
 
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import { defineConfig } from 'vite'
 
